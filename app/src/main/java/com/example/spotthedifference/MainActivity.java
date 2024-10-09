e771d36f2df979bc376ac85d28b6e42b5851f04e
QuentinDucerf <quentin.ducerf@iut-dijon.u-bourgogne.fr>
2024-10-09 15:44:38 +0200

    Ajout classe Coordonnees.java Ajout Interface ApiService.java Modification build.gradle en ajoutant Retrofit qui est utilisé pour faire les appels réseau Modification MainActivity pour la liste de coordonnées et la méthode d'envoie des coordonness au serveur

--- app/src/main/java/com/example/spotthedifference/MainActivity.java
+++ app/src/main/java/com/example/spotthedifference/MainActivity.java
@@ -8,6 +8,16 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /// <summary>
 /// Classe principale de l'application.
 /// Gère l'affichage principal, la détection des clics, et l'affichage d'un cercle rouge.
@@ -17,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private float clickX;
     private float clickY;
     private ImageView circleImageView;
+    private List<Coordonnees> listeCoordonnees;
+    private ApiService apiService;
 
     /// <summary>
     /// Méthode appelée à la création de l'activité.
@@ -28,16 +40,28 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView imageView = findViewById(R.id.imageView);
         FrameLayout layout = findViewById(R.id.frameLayout);
+        listeCoordonnees = new ArrayList<>();
 
         circleImageView = new ImageView(this);
         circleImageView.setImageResource(R.drawable.cercle_rouge);
         circleImageView.setVisibility(View.INVISIBLE);
         layout.addView(circleImageView, new FrameLayout.LayoutParams(50, 50));
 
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://url-serveur/") // Remplacer l'url
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        apiService = retrofit.create(ApiService.class);
+
+
         imageView.setOnTouchListener((v, event) -> {
             if (event.getAction() == MotionEvent.ACTION_DOWN) {
                 clickX = event.getX();
                 clickY = event.getY();
+
+                Coordonnees coord = new Coordonnees(clickX, clickY);
+                listeCoordonnees.add(coord);
 
                 circleImageView.setX(clickX - 25);
                 circleImageView.setY(clickY - 25);
@@ -46,9 +70,32 @@ public class MainActivity extends AppCompatActivity {
                 v.performClick();
 
                 Toast.makeText(MainActivity.this, "Clic détecté : X=" + clickX + ", Y=" + clickY, Toast.LENGTH_SHORT).show();
+                Coordonnees coordonnees = new Coordonnees(clickX, clickY);
+                EnvoieCoordonneesAServeur(listeCoordonnees);
                 return true;
             }
             return false;
         });
     }
+
+    private void EnvoieCoordonneesAServeur(List<Coordonnees> coordonneesList) {
+        Call<ResponseBody> call = apiService.sendCoordinates(coordonneesList);
+        call.enqueue(new Callback<ResponseBody>() {
+
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Coordonnées envoyées au serveur", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Erreur lors de l'envoi des coordonnées", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Erreur lors de l'envoi des coordonnées", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
+

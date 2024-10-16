92ef926553eae713919f5eed242301ad2fc35b57
AntoineReiterIUT <134161357+AntoineReiterIUT@users.noreply.github.com>
2024-10-16 13:58:33 +0200

    ajout du multi client cotée server

---
+++ server/API7D/Metier/SessionCodeGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API7D.Metier
+{
+    public class SessionCodeGenerator
+    {
+        private readonly HashSet<int> generatedCodes;
+        private readonly Random random;
+
+        public SessionCodeGenerator()
+        {
+            generatedCodes = new HashSet<int>(); // plus perfomant qu'une list
+            random = new Random();
+        }
+
+        public int GenerateUniqueCode()
+        {
+            int code;
+
+            // Continue generating until we find a unique code
+            do
+            {
+                code = random.Next(100000, 999999); // Génère un code à 6 chiffres
+            } while (generatedCodes.Contains(code));
+
+            generatedCodes.Add(code); // Ajoute le code à l'ensemble des codes générés
+            return code;
+        }
+
+        public void InvalidateCode(int code)
+        {
+            // Permet de libérer un code s'il n'est plus utilisé
+            generatedCodes.Remove(code);
+        }
+    }
+
+}
--- server/API7D/objet/Player.cs
+++ server/API7D/objet/Player.cs
@@ -4,5 +4,15 @@
     {
         public string PlayerId { get; set; } // Identifiant unique du joueur
         public string Name { get; set; } // Nom du joueur
+
+        // Constructeur sans paramètre requis pour la désérialisation JSON
+        public Player() { }
+
+        // Constructeur avec paramètres
+        public Player(string playerId, string name)
+        {
+            PlayerId = playerId;
+            Name = name;
+        }
     }
 }

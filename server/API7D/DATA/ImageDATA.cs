a2d6d1b547e3bfa283d942c21e7cde4373610766
antoine <antoine.reiter@iut-dijon.u-bourgogne.fr>
2024-11-13 10:06:03 +0100

    implementation BDD

--- server/API7D/DATA/ImageDATA.cs
+++ server/API7D/DATA/ImageDATA.cs
@@ -1,27 +1,76 @@
-﻿using API7D.Metier;
-using API7D.objet;
+﻿using System;
+using System.Data;
 using System.IO;
+using API7D.objet;
+using Microsoft.Data.Sqlite;
 
 namespace API7D.DATA
 {
     public class ImageDATA : IImageDATA
     {
+        private readonly string _connectionString;
 
         /// <summary>
-        /// fausse base de donnée
+        /// Initialise une nouvelle instance de ImageDATA avec une connexion SQLite.
         /// </summary>
-        /// <param name="ID"></param>
-        /// <returns></returns>
-        public string GetImagesDATA(int ID)
+        public ImageDATA()
         {
-            //a connecter a la futur BDD en attandant mettre chemin image ici
-            string path = "C:\\Users\\antoi\\Desktop\\android2\\S5_B2_Neuilcorp\\server\\API7D\\Image\\test.png";
-            return path;
+            // Connexion à une base de données SQLite locale
+            _connectionString = "Data Source=./Database/DataBase_Image.db";
         }
 
+        /// <summary>
+        /// Récupère le lien d'image correspondant à un ID donné.
+        /// </summary>
+        /// <param name="ID">L'ID de l'image à récupérer.</param>
+        /// <returns>Le chemin d'accès à l'image.</returns>
+        public string GetImagesDATA(int ID)
+        {
+            string imagePath = null;
+
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT ImageLink FROM Images WHERE ImageId = @ID";
+                using (var command = new SqliteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ID", ID);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            imagePath = reader.GetString(0); // Récupère la valeur de ImageLink
+                        }
+                    }
+                }
+            }
+
+            return imagePath ?? throw new Exception("Image not found.");
+        }
+
+        /// <summary>
+        /// Ajoute une nouvelle image dans la base de données.
+        /// </summary>
+        /// <param name="image">L'objet image contenant les informations de l'image.</param>
         public void SetImagesDATA(ImageDifference image)
         {
-            throw new NotImplementedException();
+            // a implementer au sprint 4
+            /*using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = "INSERT INTO Images (ImageId, ImagePaire, ImageLink) VALUES (@ImageId, @ImagePaire, @ImageLink)";
+                using (var command = new SqliteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ImageId", image.ImageId);
+                    command.Parameters.AddWithValue("@ImagePaire", image.ImagePaire);
+                    command.Parameters.AddWithValue("@ImageLink", image.ImageLink);
+
+                    command.ExecuteNonQuery();
+                }
+            }*/
         }
     }
 }
---
+++ server/API7D/Database/Create_Table_Images.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Images (
+    ImageId INT PRIMARY KEY,
+    ImagePaire INT NOT NULL,
+    ImageLink VARCHAR(255) NOT NULL
+);

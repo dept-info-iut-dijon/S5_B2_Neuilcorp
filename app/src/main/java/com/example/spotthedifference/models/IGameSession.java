878bef48139cd43757e569608304d26f70550943
AntoineReiterIUT <134161357+AntoineReiterIUT@users.noreply.github.com>
2024-11-14 15:35:31 +0100

    Update IGameSession.java
    modification pour raccord cotée server

--- app/src/main/java/com/example/spotthedifference/models/IGameSession.java
+++ app/src/main/java/com/example/spotthedifference/models/IGameSession.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 /**
  * Interface représentant une session de jeu, avec des méthodes pour gérer
- * l'identifiant de la session et la liste des joueurs.
+ * l'identifiant de la session, la liste des joueurs, et les états du jeu.
  */
 public interface IGameSession {
 
@@ -30,9 +30,44 @@ public interface IGameSession {
     List<Player> getPlayers();
 
     /**
-     * Supprime un joueur de la session de jeu. (en prévision de la suppression d'un joueur par l'hôte)
+     * Définit la liste des joueurs participant à la session de jeu.
+     *
+     * @param players Liste des joueurs.
+     */
+    void setPlayers(List<Player> players);
+
+    /**
+     * Supprime un joueur de la session de jeu.
      *
      * @param player Le joueur à supprimer.
      */
     void removePlayer(Player player);
+
+    /**
+     * Vérifie si la session de jeu est terminée.
+     *
+     * @return true si le jeu est terminé, sinon false.
+     */
+    boolean isGameCompleted();
+
+    /**
+     * Définit l'état de complétion de la session de jeu.
+     *
+     * @param gameCompleted true si le jeu est terminé, sinon false.
+     */
+    void setGameCompleted(boolean gameCompleted);
+
+    /**
+     * Obtient l'état du minuteur de la session de jeu.
+     *
+     * @return true si le minuteur est activé, sinon false.
+     */
+    boolean isGameTimer();
+
+    /**
+     * Définit l'état du minuteur de la session de jeu.
+     *
+     * @param gameTimer true si le minuteur est activé, sinon false.
+     */
+    void setGameTimer(boolean gameTimer);
 }
